ef12988c00426f44023ec61587847a56e481d237
smithgb <smithgb@rose-hulman.edu>
2014-05-01 19:44:16 -0400

    Command line arguments

--- SimpleWebServer/src/Main.java
+++ SimpleWebServer/src/Main.java
@@ -1,9 +1,48 @@
-
+/*
+	HAIL HYDRA!
+*/
 public class Main {
 	public static final int DEFAULT_PORT = 8000;
 	
+	/*
+		Parses the command line arguments input by the user
+		and then creates the server.
+	*/
 	public static void main(String[] args) {
-		Server s = new Server(DEFAULT_PORT, null, true);
+		// initialize flags for parsing
+		int pFlag = 0,rFlag = 0;
+
+		// intialize server defaults
+		int port = DEFAULT_PORT;
+		String path = null;
+		boolean debug = false;
+
+		// parse command line arguments
+		for(String str : args){
+			if (str.equals("-p"))
+				pFlag = 1;
+			else if (str.equals("-root"))
+				rFlag = 1;
+			else if (str.equals("-debug") || str.equals("-d"))
+				debug = true;
+			else{
+				if ((pFlag == 1) && (Integer.parseInt(str) > 0)) {
+					// set port and reset flag
+					port = (Integer.parseInt(str));
+					pFlag = 0;
+				}else if (rFlag == 1) {
+					// set root folder and reset flag
+					path = str;
+					rFlag = 0;
+				}else{
+					// print message and quit
+					System.out.println("Invlaid arguments.\n");
+					return;
+				}
+			}
+		}
+		// Spawn server
+		Server s = new Server(port, path, debug);
 	}
 
 }
