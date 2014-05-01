6826aa99a73e190269fd8a814b6f8439b51d0df2
Robbie Cooper <cooperra@rose-hulman.edu>
2014-05-01 17:56:29 -0400

    Add a default constructor to Request

--- SimpleWebServer/src/Response.java
+++ SimpleWebServer/src/Response.java
@@ -1,3 +1,8 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 
 public class Response {
 	protected String Date;
@@ -5,7 +10,7 @@ public class Response {
 	protected int ContentLength;
 	protected String Connection;
 	protected String ContentType;
-	protected String dataLoad;
+	private String dataLoad;
 	
 	public static Response parseResponse(Request r) {
 		Response response = null;
@@ -14,5 +19,28 @@ public class Response {
 		}
 		
 		return response;
+	}
+	
+	public Response() {
+		this.Date = getServerTime();
+		this.Server = Constants.SERVER_NAME;
+	}
+	
+	/* Thanks Stackoverflow! */
+	private String getServerTime() {
+	    Calendar calendar = Calendar.getInstance();
+	    SimpleDateFormat dateFormat = new SimpleDateFormat(
+	        "EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+	    dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+	    return dateFormat.format(calendar.getTime());
+	}
+	
+	public void setDataLoad(String data) {
+		this.dataLoad = data;
+		this.ContentLength = dataLoad.length();
+	}
+	
+	public String getDataLoad() {
+		return this.dataLoad;
 	}
 }
